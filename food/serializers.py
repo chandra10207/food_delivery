846329618f128f09d4ace28366e1ba9255f37350
Chandra <chandra10207@gmail.com>
2019-12-08 18:58:14 +1100

    serializers class added on food

---
+++ food/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from food.models import Food
+
+
+class FoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Food
+        fields = ("id", "name", "name", "content", "regular_price", "sale_price", "sale_price", "image", "restaurant", "owner")
