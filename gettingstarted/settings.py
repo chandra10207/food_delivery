4b26e793ff4c71ec06e2779ed95973d3e86c1871
Chandra <chandra10207@gmail.com>
2019-12-28 17:49:43 +1100

    middeleware storage updated

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -156,7 +156,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = "gettingstarted.urls"
 
 TEMPLATES = [
