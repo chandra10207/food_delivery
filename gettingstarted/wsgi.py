8654479d5f7684a31e0c289b6a2d17a9363da1bc
Chandra <chandra10207@gmail.com>
2019-12-28 18:21:44 +1100

    whitenoise added to requirements

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -12,5 +12,28 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from whitenoise import WhiteNoise
+
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root=settings.MEDIA_ROOT, prefix='media/')
+# application.add_files('/path/to/more/static/files', prefix='media/')
+
+
+# from whitenoise.django import DjangoWhiteNoise
+
+# application = WhiteNoise(
+#     DjangoWhiteNoise(get_wsgi_application()),
+#     root=settings.MEDIA_ROOT,
+#     prefix='/media/',
+# )
+
+# from whitenoise import WhiteNoise
+
+# from gettingstarted import MyWSGIApp
+
+# application = MyWSGIApp()
+# application = WhiteNoise(application, root='/path/to/static/files')
+# application.add_files('/path/to/more/static/files', prefix='more-files/')
