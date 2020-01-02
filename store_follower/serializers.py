fba756b6e6c7b7160ea8ab0658ad7eb64176d4c3
Chandra <chandra10207@gmail.com>
2020-01-02 23:34:06 +1100

    Duplicate store follower issues fixed

--- store_follower/serializers.py
+++ store_follower/serializers.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
 from store_follower.models import StoreFollower
-
+from rest_framework.validators import ValidationError
 
 class StoreFollowerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = StoreFollower
-        fields = ('user_id', 'follower_name', 'restaurant_id', 'restaurant_name', 'followed_on')
+        fields = ( 'id', 'user_id', 'follower_name', 'restaurant_id', 'restaurant_name', 'followed_on')
 
+    def create(self, validated_data):
+
+        user_id = validated_data.get('user_id')
+        restaurant_id = validated_data.get('restaurant_id')
+        # breakpoint()
+        if StoreFollower.objects.filter(user_id=user_id).filter(restaurant_id=restaurant_id).exists():
+            content = {'errors': ['Restaurant already added to the favourite list.']}
+            raise ValidationError(content)
+        else:
+            follow = StoreFollower.objects.create(**validated_data)
+        return follow
--- store_follower/views.py
+++ store_follower/views.py
@@ -3,12 +3,28 @@ from store_follower.models import StoreFollower
 from store_follower.serializers import StoreFollowerSerializer
 from rest_framework import generics
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from rest_framework.validators import ValidationError
 
 
 # Create your views here.
 
 class StoreFollowerListAPI(generics.ListCreateAPIView):
-    queryset = StoreFollower.objects.all()
+
+    def get_queryset(self):
+        params = self.request.query_params
+        # breakpoint()
+        user_id = self.request.query_params.get('user_id')
+        if user_id:
+            if User.objects.filter(id=user_id).exists():
+                queryset = StoreFollower.objects.filter(user_id=user_id)
+            else:
+                content = {'errors': 'user id not exist'}
+                raise ValidationError(content)
+        else:
+            queryset = StoreFollower.objects.all()
+        return queryset
+    # queryset = StoreFollower.objects.all()
     serializer_class = StoreFollowerSerializer
 
 
