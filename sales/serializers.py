5c24f33fd462989c76adb3955b3d2a25e4af7412
Chandra <chandra10207@gmail.com>
2020-02-02 18:48:02 +1100

    items details added on order list api

--- sales/serializers.py
+++ sales/serializers.py
@@ -3,13 +3,13 @@ from rest_framework.relations import StringRelatedField
 from sales.models import Order
 from sales.models import OrderItem
 from sales.models import OrderItemMeta
-from food.serializers import JustFoodSerializer
+from food.serializers import JustFoodSerializer,FoodSerializer
 from restaurant.serializers import JustRestaurantSerializer
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
 
-    # food_id = FoodSerializer( read_only=True)
+    food_id = FoodSerializer( read_only=True)
 
     class Meta:
         # model = models.CustomUser
@@ -20,13 +20,14 @@ class OrderItemSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
 
     # restaurant = JustRestaurantSerializer(read_only=True)
+    order_items = OrderItemSerializer(many=True,read_only=True)
 
     # food_id = FoodSerializer( read_only=True)
 
     class Meta:
         # model = models.CustomUser
         model = Order
-        fields = ('id','user_id', 'order_total', 'seller_total', 'seller_id', 'order_status','created_on','completed_on')
+        fields = ('id','user_id', 'order_total', 'seller_total', 'seller_id', 'order_status', 'order_items', 'created_on','completed_on')
 
 
 class OrderDetailSerializer(serializers.ModelSerializer):
