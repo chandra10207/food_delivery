a0d2a6215ea34e440eca9a8dfb3d434b703cc9ac
Chandra <chandra10207@gmail.com>
2020-02-09 13:20:22 +1100

    Name field added on review

--- review/serializers.py
+++ review/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
 from rest_framework.validators import ValidationError
 from review.models import Review
+from django.contrib.auth.models import User
 
 class ReviewSerializer(serializers.ModelSerializer):
+
+    name = serializers.SerializerMethodField()
+
+    def get_name(self, obj):
+        # breakpoint()
+        # user = User.objects.get(pk=obj.user_id.id)
+        return "{}".format(obj.user_id.first_name)
+
     class Meta:
         model = Review
-        fields = ('id','user_id', 'restaurant_id', 'rating', 'created_on', 'review_title', 'review_text')
+        fields = ('id','user_id', 'restaurant_id', 'rating', 'created_on', 'review_title', 'review_text','name')
 
     def create(self, validated_data):
         user_id = validated_data.get('user_id')
