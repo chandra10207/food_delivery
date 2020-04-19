b1d446c57b5f3303ef808868e666e281c5942d0f
Chandra <chandra10207@gmail.com>
2020-04-19 19:27:04 +1000

    New changes

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+# from django.test import TestCase
+# from django.urls import reverse, resolve
+# # from .views import home, board_topics, new_topic
+# from restaurant.models import Restaurant
+#
+# class RestaurantTests(TestCase):
+#     def setUp(self):
+#         Restaurant.objects.create(name='Django', content='Some Text',owner=1)
+#
+#     def test_board_topics_view_success_status_code(self):
+#         url = reverse('boards:board_topics', kwargs={'pk': 5})
+#         response = self.client.get(url)
+#         self.assertEquals(response.status_code, 200)
 
-# Create your tests here.
+
+from django.test import TestCase
+from restaurant.models import Restaurant
+
+class RestaurantTestCase(TestCase):
+    def setUp(self):
+        Restaurant.objects.create(name='Mecca', content='Some Text',owner=1)
+
+    def test_restaurant_name(self):
+        """Animals that can speak are correctly identified"""
+        restaurant_name = Restaurant.objects.get(name="Mecca")
+        self.assertEqual(restaurant_name, 'Mecca')
--- sales/tests.py
+++ sales/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from sales.models import Order
+from django.contrib.auth.models import User
+from restaurant.models import Restaurant
 
-# Create your tests here.
+class OrderTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(
+        username='javed', email='javed@javed.com', password='my_secret')
+        restaurant = Restaurant.objects.create(name='Restaurant 1', content='Some description about restaurant',owner=user)
+        Order.objects.create(user_id=user, order_total=1235, seller_id=restaurant)
+
+    def test_restaurant_name(self):
+        order = Order.objects.get(order_total =1235)
+        self.assertEqual(order.seller_id.name, "Restaurant 1")
+
+
+
